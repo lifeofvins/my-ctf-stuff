d20e864b67700a0d42dc5a4e7875d9eef2d21929
lifeofvins <vincenzo.sagristano@studenti.polito.it>
2022-10-29 18:57:36 +0200

    probably improved but not working yet

--- vigenere-cipher/vigenere.py
+++ vigenere-cipher/vigenere.py
@@ -4,13 +4,19 @@
 # I’m not sure what this means, but it was left lying around: blorpy
 # gwox{RgqssihYspOntqpxs}
 
+from string import *
+
 keys = {0: 1, 1: 11, 2: 14, 3: 17, 4: 15, 5: 24}
 enc = "gwox{RgqssihYspOntqpxs}"
 dec = ""
 i = 0
 for letter in enc:
+    if(not(letter in ascii_uppercase or letter in ascii_lowercase)):
+        dec += letter
+        continue
     dec += chr((ord(letter) - keys[i % 6]))
     i += 1
+print()
 print(dec)
 print("CTFlearn{" + dec + "}")
 print("CTFlearn{" + dec[4:] + "}")
